447529aed7063ecc9d82c1314fdfbbeea747bcd8
Zakir-Ansari <azakir780@gmail.com>
2024-09-22 13:23:40 +0530

    updating shell app environments

--- todo-monorepo-app/apps/shell-app/src/environments/environment.prod.ts
+++ todo-monorepo-app/apps/shell-app/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   env: 'prod',
   remoteUrls: {
-    'create-todo': 'http://localhost:4201',
+    'create-todo': 'https://create-todo-nine.vercel.app',
   },
 };
